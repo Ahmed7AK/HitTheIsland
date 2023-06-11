96fd65ac94d0c11e7631f584fbcae0b283f6a118
Ahmed AK <ahmedakheirallah@gmail.com>
2023-06-11 15:20:08 +0400

    Set up main.py

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import pygame 
-# hmmm
+
+class HitTheIsland:
+    def __init__(self):
+
+        # Screen
+        self.screen_size = (337, 600)
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+        # Title and Icon
+        pygame.display.set_caption("Hit The Island")
+        # Icon
+
+        # Variables 
+        self.WHITE = (255, 255, 255)
+
+        # Loop
+        self.running = True
+        while self.running:
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    self.running = False
+                    break
+            HitTheIsland.draw(self)
+
+    def draw(self):
+        self.screen.fill(self.WHITE)
+        pygame.display.update()
+                    
+
+
+
+game = HitTheIsland()
